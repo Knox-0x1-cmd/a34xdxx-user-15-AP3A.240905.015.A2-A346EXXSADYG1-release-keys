aa2521954b937695dab2f0994c7fae1b410a8cfd
Knox-0x1-cmd <helloahmedhashem@gmail.com>
2026-09-15 12:54:23 +0000

    dump: aosp-device-tree: import dump for a34x   Firmware: https://s03.ooo/v2/IxJCDiMnLj8vAy49Iy8zHi8WHisNMR4lFzssIDs2ByAzMUEgOzEUQDMQMCMBOyw/LwcCKx4wOzkzCC8UNTAuIA0LLkA1CAYnPCVBLCMXPkA7LBUUEgdBIxJABzk1QCAzHgcCIxILLA4BGy4RMwgHPRIIBhYBJwofIwgKOQMXFBADFi8hARshASMbBkIeCzwGHgM8FCMkNDEjCxVACTghHzInDQYuFx8rIwAvJCFAMxE8EQY5NRshDjURPho0ES8kNRYhOR44LwsJEQc5NBsjKyEAPg4hAAYsLjgeLDQRHg4yJTMsMiUNKzwAPh8mGzk5FyUzDTUHQQEuByADISxBIwMnOUANQAokMzYCICMXPjkDFzA1CS8pDTsxHhwzQAZCOzEHHjMsNCsNMT4eODAeHgk7CCQeFiEHODEvGxcvPiAjJA0AMy8pPDsxBwgXOzQGOAAhKTw/FD01OCBCJhshOzQxAgo4JDMSMycfBB4WHjguLCwRAzYCMSElIjkuLzMfIwAUICELLCwjOAZCIwM0HQEDOwYBFjs5Iw0TEw==
    Signed-off-by: Knox-0x1-cmd <helloahmedhashem@gmail.com>

---
+++ aosp-device-tree/AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_a34x.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a34x-user \
+    lineage_a34x-userdebug \
+    lineage_a34x-eng
---
+++ aosp-device-tree/lineage_a34x.mk
@@ -0,0 +1,26 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from a34x device
+$(call inherit-product, device/samsung/a34x/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := a34x
+PRODUCT_NAME := lineage_a34x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A346E
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="a34xdxx-user 15 AP3A.240905.015.A2 A346EXXSADYG1 release-keys" \
+    BuildFingerprint=samsung/a34xdxx/a34x:15/AP3A.240905.015.A2/A346EXXSADYG1:user/release-keys
